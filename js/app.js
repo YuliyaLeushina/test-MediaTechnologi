134077919b9e1590fbe4647c97a7ff451e3ce73c
Leushina Ylia <leushinayliya@gmail.com>
2023-01-29 21:19:38 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,386 @@
+(() => {
+  "use strict";
+  const t = {};
+  let e = !0,
+    s = (t = 500) => {
+      document.documentElement.classList.contains("lock") ? o(t) : i(t);
+    },
+    o = (t = 500) => {
+      let s = document.querySelector("body");
+      if (e) {
+        let o = document.querySelectorAll("[data-lp]");
+        setTimeout(() => {
+          for (let t = 0; t < o.length; t++) {
+            o[t].style.paddingRight = "0px";
+          }
+          (s.style.paddingRight = "0px"),
+            document.documentElement.classList.remove("lock");
+        }, t),
+          (e = !1),
+          setTimeout(function () {
+            e = !0;
+          }, t);
+      }
+    },
+    i = (t = 500) => {
+      let s = document.querySelector("body");
+      if (e) {
+        let o = document.querySelectorAll("[data-lp]");
+        for (let t = 0; t < o.length; t++) {
+          o[t].style.paddingRight =
+            window.innerWidth -
+            document.querySelector(".wrapper").offsetWidth +
+            "px";
+        }
+        (s.style.paddingRight =
+          window.innerWidth -
+          document.querySelector(".wrapper").offsetWidth +
+          "px"),
+          document.documentElement.classList.add("lock"),
+          (e = !1),
+          setTimeout(function () {
+            e = !0;
+          }, t);
+      }
+    };
+  t.popup = new (class {
+    constructor(t) {
+      let e = {
+        logging: !0,
+        init: !0,
+        attributeOpenButton: "data-popup",
+        attributeCloseButton: "data-close",
+        fixElementSelector: "[data-lp]",
+        youtubeAttribute: "data-youtube",
+        youtubePlaceAttribute: "data-youtube-place",
+        setAutoplayYoutube: !0,
+        classes: {
+          popup: "popup",
+          popupContent: "popup__content",
+          popupActive: "popup_show",
+          bodyActive: "popup-show",
+        },
+        focusCatch: !0,
+        closeEsc: !0,
+        bodyLock: !0,
+        bodyLockDelay: 500,
+        hashSettings: { location: !0, goHash: !0 },
+        on: {
+          beforeOpen: function () {},
+          afterOpen: function () {},
+          beforeClose: function () {},
+          afterClose: function () {},
+        },
+      };
+      (this.isOpen = !1),
+        (this.targetOpen = { selector: !1, element: !1 }),
+        (this.previousOpen = { selector: !1, element: !1 }),
+        (this.lastClosed = { selector: !1, element: !1 }),
+        (this._dataValue = !1),
+        (this.hash = !1),
+        (this._reopen = !1),
+        (this._selectorOpen = !1),
+        (this.lastFocusEl = !1),
+        (this._focusEl = [
+          "a[href]",
+          'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+          "button:not([disabled]):not([aria-hidden])",
+          "select:not([disabled]):not([aria-hidden])",
+          "textarea:not([disabled]):not([aria-hidden])",
+          "area[href]",
+          "iframe",
+          "object",
+          "embed",
+          "[contenteditable]",
+          '[tabindex]:not([tabindex^="-"])',
+        ]),
+        (this.options = {
+          ...e,
+          ...t,
+          classes: { ...e.classes, ...t?.classes },
+          hashSettings: { ...e.hashSettings, ...t?.hashSettings },
+          on: { ...e.on, ...t?.on },
+        }),
+        this.options.init && this.initPopups();
+    }
+    initPopups() {
+      this.popupLogging("Проснулся"), this.eventsPopup();
+    }
+    eventsPopup() {
+      document.addEventListener(
+        "click",
+        function (t) {
+          const e = t.target.closest(`[${this.options.attributeOpenButton}]`);
+          if (e)
+            return (
+              t.preventDefault(),
+              (this._dataValue = e.getAttribute(
+                this.options.attributeOpenButton
+              )
+                ? e.getAttribute(this.options.attributeOpenButton)
+                : "error"),
+              "error" !== this._dataValue
+                ? (this.isOpen || (this.lastFocusEl = e),
+                  (this.targetOpen.selector = `${this._dataValue}`),
+                  (this._selectorOpen = !0),
+                  void this.open())
+                : void this.popupLogging(
+                    `Ой ой, не заполнен атрибут у ${e.classList}`
+                  )
+            );
+          return t.target.closest(`[${this.options.attributeCloseButton}]`) ||
+            (!t.target.closest(`.${this.options.classes.popupContent}`) &&
+              this.isOpen) ||
+            t.target.closest(".radio__group")
+            ? (t.target.closest(".radio__group") &&
+                (document.querySelector(".header__link-locations").textContent =
+                  t.target.closest(".radio__group").textContent),
+              t.preventDefault(),
+              void this.close())
+            : void 0;
+        }.bind(this)
+      ),
+        document.addEventListener(
+          "keydown",
+          function (t) {
+            if (
+              this.options.closeEsc &&
+              27 == t.which &&
+              "Escape" === t.code &&
+              this.isOpen
+            )
+              return t.preventDefault(), void this.close();
+            this.options.focusCatch &&
+              9 == t.which &&
+              this.isOpen &&
+              this._focusCatch(t);
+          }.bind(this)
+        ),
+        this.options.hashSettings.goHash &&
+          (window.addEventListener(
+            "hashchange",
+            function () {
+              window.location.hash
+                ? this._openToHash()
+                : this.close(this.targetOpen.selector);
+            }.bind(this)
+          ),
+          window.addEventListener(
+            "load",
+            function () {
+              window.location.hash && this._openToHash();
+            }.bind(this)
+          ));
+    }
+    open(t) {
+      if (
+        (t &&
+          "string" == typeof t &&
+          "" !== t.trim() &&
+          ((this.targetOpen.selector = t), (this._selectorOpen = !0)),
+        this.isOpen && ((this._reopen = !0), this.close()),
+        this._selectorOpen ||
+          (this.targetOpen.selector = this.lastClosed.selector),
+        this._reopen || (this.previousActiveElement = document.activeElement),
+        (this.targetOpen.element = document.querySelector(
+          this.targetOpen.selector
+        )),
+        this.targetOpen.element)
+      ) {
+        if (
+          this.targetOpen.element.hasAttribute(this.options.youtubeAttribute)
+        ) {
+          const t = `https://www.youtube.com/embed/${this.targetOpen.element.getAttribute(
+              this.options.youtubeAttribute
+            )}?rel=0&showinfo=0&autoplay=1`,
+            e = document.createElement("iframe");
+          e.setAttribute("allowfullscreen", "");
+          const s = this.options.setAutoplayYoutube ? "autoplay;" : "";
+          e.setAttribute("allow", `${s}; encrypted-media`),
+            e.setAttribute("src", t),
+            this.targetOpen.element.querySelector(
+              `[${this.options.youtubePlaceAttribute}]`
+            ) &&
+              this.targetOpen.element
+                .querySelector(`[${this.options.youtubePlaceAttribute}]`)
+                .appendChild(e);
+        }
+        this.options.hashSettings.location &&
+          (this._getHash(), this._setHash()),
+          this.options.on.beforeOpen(this),
+          this.targetOpen.element.classList.add(
+            this.options.classes.popupActive
+          ),
+          document.body.classList.add(this.options.classes.bodyActive),
+          this._reopen ? (this._reopen = !1) : s(),
+          this.targetOpen.element.setAttribute("aria-hidden", "false"),
+          (this.previousOpen.selector = this.targetOpen.selector),
+          (this.previousOpen.element = this.targetOpen.element),
+          (this._selectorOpen = !1),
+          (this.isOpen = !0),
+          setTimeout(() => {
+            this._focusTrap();
+          }, 50),
+          document.dispatchEvent(
+            new CustomEvent("afterPopupOpen", { detail: { popup: this } })
+          ),
+          this.popupLogging("Открыл попап");
+      } else
+        this.popupLogging(
+          "Ой ой, такого попапа нет. Проверьте корректность ввода. "
+        );
+    }
+    close(t) {
+      t &&
+        "string" == typeof t &&
+        "" !== t.trim() &&
+        (this.previousOpen.selector = t),
+        this.isOpen &&
+          e &&
+          (this.options.on.beforeClose(this),
+          this.targetOpen.element.hasAttribute(this.options.youtubeAttribute) &&
+            this.targetOpen.element.querySelector(
+              `[${this.options.youtubePlaceAttribute}]`
+            ) &&
+            (this.targetOpen.element.querySelector(
+              `[${this.options.youtubePlaceAttribute}]`
+            ).innerHTML = ""),
+          this.previousOpen.element.classList.remove(
+            this.options.classes.popupActive
+          ),
+          this.previousOpen.element.setAttribute("aria-hidden", "true"),
+          this._reopen ||
+            (document.body.classList.remove(this.options.classes.bodyActive),
+            s(),
+            (this.isOpen = !1)),
+          this._removeHash(),
+          this._selectorOpen &&
+            ((this.lastClosed.selector = this.previousOpen.selector),
+            (this.lastClosed.element = this.previousOpen.element)),
+          this.options.on.afterClose(this),
+          setTimeout(() => {
+            this._focusTrap();
+          }, 50),
+          this.popupLogging("Закрыл попап"));
+    }
+    _getHash() {
+      this.options.hashSettings.location &&
+        (this.hash = this.targetOpen.selector.includes("#")
+          ? this.targetOpen.selector
+          : this.targetOpen.selector.replace(".", "#"));
+    }
+    _openToHash() {
+      let t = document.querySelector(
+        `.${window.location.hash.replace("#", "")}`
+      )
+        ? `.${window.location.hash.replace("#", "")}`
+        : document.querySelector(`${window.location.hash}`)
+        ? `${window.location.hash}`
+        : null;
+      document.querySelector(`[${this.options.attributeOpenButton}="${t}"]`) &&
+        t &&
+        this.open(t);
+    }
+    _setHash() {
+      history.pushState("", "", this.hash);
+    }
+    _removeHash() {
+      history.pushState("", "", window.location.href.split("#")[0]);
+    }
+    _focusCatch(t) {
+      const e = this.targetOpen.element.querySelectorAll(this._focusEl),
+        s = Array.prototype.slice.call(e),
+        o = s.indexOf(document.activeElement);
+      t.shiftKey && 0 === o && (s[s.length - 1].focus(), t.preventDefault()),
+        t.shiftKey || o !== s.length - 1 || (s[0].focus(), t.preventDefault());
+    }
+    _focusTrap() {
+      const t = this.previousOpen.element.querySelectorAll(this._focusEl);
+      !this.isOpen && this.lastFocusEl
+        ? this.lastFocusEl.focus()
+        : t[0].focus();
+    }
+    popupLogging(t) {
+      this.options.logging &&
+        (function (t) {
+          setTimeout(() => {
+            window.FLS && console.log(t);
+          }, 0);
+        })(`[Попапос]: ${t}`);
+    }
+  })({});
+  let n = !1;
+  setTimeout(() => {
+    if (n) {
+      let t = new Event("windowScroll");
+      window.addEventListener("scroll", function (e) {
+        document.dispatchEvent(t);
+      });
+    }
+  }, 0);
+  document.querySelectorAll(".menu__item").forEach((t) => {
+    t.addEventListener("mouseenter", function () {
+      if (!(this.children.length > 1)) return !1;
+      this.children[1].classList.add("_show-header-el"),
+        this.classList.add("_red");
+    }),
+      t.addEventListener("mouseleave", function () {
+        this.children.length > 1 &&
+          (this.children[1].classList.remove("_show-header-el"),
+          this.classList.remove("_red"));
+      });
+  });
+  const l = document.querySelector(".header__link-locations"),
+    a = document.querySelectorAll(".radio__group");
+  for (const t of a)
+    t.addEventListener("input", function () {
+      l.textContent = t.textContent;
+    });
+  let r, c;
+  function h(t) {
+    a.forEach((e) => {
+      (c = t.toUpperCase()),
+        e.textContent.toUpperCase().indexOf(c) > -1
+          ? (e.style.display = "")
+          : (e.style.display = "none");
+    });
+  }
+  (r = document.getElementById("myInput")),
+    r.addEventListener("input", function () {
+      (r.value || "" === r.value) && h(r.value);
+    });
+  document
+    .querySelector(".popup__btn-location")
+    .addEventListener("click", function () {
+      fetch(
+        "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=46.226.227.20",
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Token 1f0c02c89314eb1ce03256887629fe1515ecf7a6",
+          },
+        }
+      )
+        .then((t) => t.json())
+        .then((t) => {
+          t && ((r.value = t.location.data.city), h(r.value));
+        })
+        .catch((t) => console.log("error", t));
+    }),
+    (window.FLS = !0),
+    (function (t) {
+      let e = new Image();
+      (e.onload = e.onerror =
+        function () {
+          t(2 == e.height);
+        }),
+        (e.src =
+          "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
+    })(function (t) {
+      let e = !0 === t ? "webp" : "no-webp";
+      document.documentElement.classList.add(e);
+    });
+})();
